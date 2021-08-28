c9e05d76b14a7ce4758ecbd75b6d79afab905dec
hwangseonbi <on91716@gmail.com>
2021-08-28 15:34:09 +0900

    스프링 적용 - IoC 제어 부분을 Spring Bean을 통해 관리하도록 수정

--- src/main/java/com/example/demo/MemberApp.java
+++ src/main/java/com/example/demo/MemberApp.java
@@ -3,14 +3,16 @@ package com.example.demo;
 import com.example.demo.member.Grade;
 import com.example.demo.member.Member;
 import com.example.demo.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     //psvm
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        MemberService memberService = appConfig.memberService();
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
