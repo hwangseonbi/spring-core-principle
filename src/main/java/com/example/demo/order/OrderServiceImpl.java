bd75bf394eb111b4cd1efdc5dc9b8573939bc674
hwangseonbi <on91716@gmail.com>
2021-08-28 14:21:14 +0900

    DIP 원칙에 위배되는 지점

--- src/main/java/com/example/demo/order/OrderServiceImpl.java
+++ src/main/java/com/example/demo/order/OrderServiceImpl.java
@@ -1,8 +1,6 @@
 package com.example.demo.order;
 
 import com.example.demo.discount.DiscountPolicy;
-import com.example.demo.discount.FixDiscountPolicy;
-import com.example.demo.discount.RateDiscountPolicy;
 import com.example.demo.member.Member;
 import com.example.demo.member.MemberRepository;
 import com.example.demo.member.MemoryMemberRepository;
@@ -10,8 +8,7 @@ import com.example.demo.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private DiscountPolicy discountPolicy; //대신 주입 해줘야함.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
