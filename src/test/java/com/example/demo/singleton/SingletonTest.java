de7bdb45a92245f41384c381f25cf5deeed59d02
hwangseonbi <on91716@gmail.com>
2021-09-01 09:39:22 +0900

    Spring 싱글톤 테스트 - 싱글톤 패턴을 적용한 객체 사용

--- src/test/java/com/example/demo/singleton/SingletonTest.java
+++ src/test/java/com/example/demo/singleton/SingletonTest.java
@@ -25,4 +25,20 @@ public class SingletonTest {
 
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        //참조값이 다른 것을 확인
+        System.out.println("singletonService1 = "+singletonService1);
+        System.out.println("singletonService2 = "+singletonService2);
+
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+        //isEqualto vs isSameAs :
+        // same
+        // equals 메서드
+    }
 }
