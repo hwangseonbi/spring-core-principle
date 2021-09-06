61aac6a2ce49c88646173f2f3593c2279366ff95
hwangseonbi <on91716@gmail.com>
2021-09-06 12:10:33 +0900

    Spring @ComponentScan을 통한 자동 Bean 등록 및 @Autowired를 통한 의존관계 자동 설정

--- src/main/java/com/example/demo/AutoAppConfig.java
+++ src/main/java/com/example/demo/AutoAppConfig.java
@@ -7,6 +7,12 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        /*
+         * 컴포넌트 스캔을 했을 때 Bean으로 등록하지 않을 것을 찾는다.
+         * Configuration안에는 Component가 붙어있다.
+         * 실무에서는 굳이 이렇게 안한다.
+         * 예제코드 살리기위해서 이렇게 한것.
+        */
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
